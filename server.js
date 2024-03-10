0e651691c15cc628b218b0ca4ab819644641a622
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-03-10 15:11:20 +0530

    Shift mongodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("WELCOME TO MY HOTEL");
@@ -14,6 +16,6 @@ const MenuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", MenuItemRoutes);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 3000");
 });
